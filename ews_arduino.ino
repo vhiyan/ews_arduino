66bf98f24a325e871c59c7858d467a2bac63019c
vhiyan <kristiawan.devianto@gmail.com>
2020-10-12 10:31:20 +0700

    update integrasi sabtu

--- ews_arduino.ino
+++ ews_arduino.ino
@@ -13,9 +13,9 @@
 #define b2 8
 #define b3 16
 #define b4 24
+#define jmlNodeid
 
-
-static const int RXPin = 3, TXPin = 4;
+static const int RXPin = 2, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 double longitude,latitude;
 
@@ -26,7 +26,7 @@ unsigned long currentTime[2], previousTime[2];
 unsigned int state =0;
 
 int k=0;
-char pass[7] = {'_','_','_','_','_','_','_'};
+char pass[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //four columns
 //define the cymbols on the buttons of the keypads
@@ -36,14 +36,14 @@ char hexaKeys[ROWS][COLS] = {
   {'7','8','9'},
   {'*','0','#'}
 };
-byte rowPins[ROWS] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {6, 7, 8, 9}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {10, 11, 12}; //connect to the column pinouts of the keypad
 bool huruf = false;
 //initialize an instance of class NewKeypad
 bool adapass = false;
 char key;
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-
+String kirim="";
 
 String inputString = ""; // a String to hold incoming data
 bool stringComplete = false; // whether the string is complete
@@ -63,31 +63,33 @@ Serial.begin(9600);
 ss.begin(GPSBaud);  
 
  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
-/// lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+ lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+lux =read_lux();
   char customKey = customKeypad.getKey();
   if(customKey!='\0'){
-     if(customKey=='#'&& k>5 && state ==0){
+     if(customKey=='#' && state ==0){
       state = 1;
+      kirim = (String)pass;
+      kirim.trim();
       Serial.print("DAFTAR#");
-      Serial.print((String)pass);
+      Serial.print(kirim);
       Serial.print("#");
       Serial.print(get_long());
       Serial.print("#");
       Serial.println(get_lat());
       //enter
      }
-     else if(customKey=='#' && state==1){
+     else if(customKey=='*' && state==1){
       Serial.println("CHANGE");
       k=0;
-      for(int i =0 ;i<7;i++)
+      for(int i =0 ;i<9;i++)
       {
-      pass[i] ='_';
+      pass[i] =' ';
       }
        state=0;
      }
@@ -95,9 +97,9 @@ void loop() {
      else if(customKey=='*' && state == 0)
      {
       k=0;
-      for(int i =0 ;i<7;i++)
+      for(int i =0 ;i<9;i++)
       {
-      pass[i] ='_';
+      pass[i] =' ';
       }
      }
      else
@@ -105,7 +107,7 @@ void loop() {
       if(state==0){
       pass[k]=customKey;
       k++;
-      if(k>6)k=6;
+      if(k>8)k=8;
       while(customKeypad.getKey()!='\0');
       }}
       
@@ -121,7 +123,7 @@ if (timer(0,500)){
   display.clearDisplay();
   display.setCursor(0, b1);
   display.println("INPUT DEVICE ID:");
-  for(int i=0;i<7;i++)
+  for(int i=0;i<9;i++)
   {
   display.setCursor(0+(i*8), b2);
   display.println(pass[i]);
@@ -131,24 +133,25 @@ if (timer(0,500)){
 
 break;
 case 1:
-//lux =read_lux();
-  if (timer(0,2000)){  
+//
+  if (timer(0,1000)){  
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.clearDisplay();
-  display.setCursor(0, b1);
+  display.setCursor(0, b2);
   display.print("lux meter : ");
   display.println(lux);   
-  for(int i=0;i<7;i++)
+  for(int i=0;i<9;i++)
   {
-  display.setCursor(0+(i*8), b2);
+  display.setCursor(0+(i*8), b1);
   display.println(pass[i]);
   }
-   
+  display.setCursor(0, b3);
+  display.print("long:");
+  display.println(get_long());
   display.setCursor(0, b4);
-  display.print("# utk ganti id ");
-  
-  
+  display.print("lat:");
+  display.println(get_lat()); 
   display.display();
 }
 
@@ -166,9 +169,9 @@ if(stringComplete){
     if (inputString == "DAFTAR"){
       Serial.println("CHANGE");
       k=0;
-      for(int i =0 ;i<7;i++)
+      for(int i =0 ;i<9;i++)
       {
-      pass[i] ='_';
+      pass[i] =' ';
       }
        state=0;
     }
